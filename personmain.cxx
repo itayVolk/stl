0aba4391e5c124fc3ee9eb556c22c8d77520481b
ItayVolk <ivolk@scu.edu>
2025-10-31 22:44:05 -0700

    Simplified for loops, made sorter use functional pointer, simplified variable definitions.

--- personmain.cxx
+++ personmain.cxx
@@ -20,6 +20,12 @@ struct CmpLessBMI {
 	const bool operator()(Person x, double y) {return BMI(x.getWeight(), x.getHeight()) < y; }
 };
 
+bool sorter(const Person& a, const Person& b){
+	if (a.getGender() != b.getGender())
+		return a.getGender() < b.getGender();
+	return a.getLName() < b.getLName();
+}
+
 int main(int argc, char *argv[]) {
     std::vector<Person> plist;
     Person p;
@@ -35,8 +41,6 @@ int main(int argc, char *argv[]) {
 
     while (std::cin >> p)
         plist.push_back(p);
-
-	vector<Person>::iterator it;
 	/*
 	 * Students to do:
 	 * Counter the number of records fall into each of the BMI range. Compute the proportion.  Output the percentage for each range. Use “std::map” to count for each range.
@@ -53,52 +57,37 @@ int main(int argc, char *argv[]) {
 	 */
 
 	 
-	 cout << "\n-- BMI Category Statistics --" << endl;
-	 map<string, int> bmiCounts;
-	 int totalRecords = 0;
-	 double personBmi = 0;
-	 for (vector<Person>::iterator it = plist.begin(); it!=plist.end(); ++it){
-		personBmi = BMI (it->getWeight(), it->getHeight());
+	cout << "BMI category Statistics:" << endl;
+	map<string, int> bmiCounts;
+	int totalRecords = 0;
+	for (auto per : plist){
 		totalRecords++;
-		for (const auto& category : BMICategory){
-			if(personBmi <= category.first){
+		for (auto category : BMICategory){
+			if(BMI(per.getWeight(), per.getHeight()) <= category.first){
 				bmiCounts[category.second]++;
 				break;
 			}
 		}
-	 }
+	}
 	cout << fixed << setprecision(2);
-	double proportion;
-	 for (map<string, int>::iterator it = bmiCounts.begin(); it !=bmiCounts.end(); ++it){
-		proportion = 0;
-		if (totalRecords > 0) proportion = (static_cast <double>(it->second) / totalRecords) * 100.0;
-		cout << it->first << ": " << it->second << " records (" << proportion << "%)" << endl;
-	 }
+	for (auto pair : bmiCounts){
+		cout << pair.first << ": " << pair.second << " records (" << pair.second*100.0/totalRecords << "%)" << endl;
+	}
 
-
-
-	 sort(plist.begin(), plist.end(), [](const Person& a, const Person& b){
-		if (a.getGender() != b.getGender()){
-			return a.getGender() < b.getGender();
-		}
-		return a.getLName() < b.getLName();
-	 });
-	 cout << "\n Sorting by Gender Then Last Name: " << endl;
-	 for (vector<Person>::iterator it = plist.begin(); it != plist.end(); ++it){
-		cout << it->getFName() << " " << it->getLName() << endl;
-	 }
-
-
+	cout << "Sorting by gender then last name:" << endl;
+	sort(plist.begin(), plist.end(), sorter);
+	for (auto per : plist){
+		cout << per << endl;
+	}
 
 	cout << "First person at or taller than the target height:" << endl;
-	if ((it = find_if(plist.begin(), plist.end(), [targetHeight](Person x) {return x.getHeight() >= targetHeight; })) != plist.end())
+	auto it = find_if(plist.begin(), plist.end(), [targetHeight](Person x){return x.getHeight() >= targetHeight;});
+	if (it != plist.end())
 		cout << *it << ": " << setprecision(2) << it->getHeight() << " >= " << targetHeight << endl;
 	else
 		cout << "None." << endl;
 
-
-
-	cout << "Person at or more than the target weight and of the target gender:" << endl;
+	cout << "First person at or more than the target weight and of the target gender:" << endl;
 	if ((it = find_if(plist.begin(), plist.end(), [targetWeight, targetGender](Person x) { return x.getWeight() >= targetWeight && x.getGender() == targetGender; })) != plist.end())
 		cout << *it << ": " << setprecision(2) << it->getWeight() << " >= " << targetWeight << " && " << it->getGender() << " == " << targetGender << endl;
 	else
